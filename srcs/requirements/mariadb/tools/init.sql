bd4cd6edb00805a65a6d57d065edcb1a1b97bd5d
noalexan <noalexan@student.42nice.fr>
2023-06-06 17:04:39 +0200

    some updates

---
+++ srcs/requirements/mariadb/tools/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `wordpress`;
+CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES ON `wordpress` . * TO 'wordpress';
+FLUSH PRIVILEGES;
